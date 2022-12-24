0d50344b3cab5afc6b5d5b018431266681324588
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-23 18:58:06 -0600

    refactored navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
-const NavBar = () => {
+function NavBar() {
+
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    }
+    // #282c34
   return (
-    <nav className='fixed w-full left-0 top-0 z-[999]'>
-        <div className='flex items-center justify-between bg-[#282c34]'>
-            <div className='mx-7'>
-                <h4 className='text-4xl uppercase font-bold text-white'>
-                    No<span>&euml;</span>l
-                </h4>
+    <div className="w-full h-[80px] bg-[#282c34]">
+        <div className="max-w-[1240px] mx-auto px-4 flex justify-between items-center h-full">
+            <p className="text-white text-3xl">
+                NO<span>&Euml;</span>L
+            </p>
+            <div className="hidden md:flex">
+                <ul className="flex items-center">
+                    <li className="px-5">
+                        <a className="uppercase text-white hover:underline" href="#home">Home</a>
+                    </li>
+                    <li className="px-5">
+                        <a className="uppercase text-white hover:underline" href="#albums">Albums</a>
+                    </li>
+                    <li className="px-5">
+                        <a className="uppercase text-white hover:underline" href="#contact">contact</a>
+                    </li>
+                </ul>
             </div>
-            <div className='text-gray-500 md:block hidden px-7 py-2 font-medium bg-white rounded-bl-full'>
-                <ul className='flex items-center gap-1 py-2 text-lg'>
-                    <li className='px-5 uppercase hover:text-[#0b1596]'>
-                        <a href="#home">Home</a>
+            <div 
+                onClick={handleNav} 
+                className='block md:hidden'
+            >
+                {nav ? <AiOutlineClose size={30} className='text-white' /> : <AiOutlineMenu size={30} className='text-white'/>}
+            </div>
+            <div 
+                className={
+                    nav 
+                    ? 'w-full absolute top-[80px] left-0 flex justify-center text-center text-white bg-[#282c34] md:hidden'
+                    : 'absolute left-[-100%] text-white md:hidden'
+                }
+            >
+                <ul>
+                    <li className="py-5">
+                        <a className="uppercase text-white hover:underline" href="#home">Home</a>
                     </li>
-                    <li className='px-5 uppercase hover:text-[#0b1596]'>
-                        <a href="#albums">albums</a>
+                    <li className="py-5">
+                        <a className="uppercase text-white hover:underline" href="#albums">Albums</a>
                     </li>
-                    <li className='px-5 uppercase hover:text-[#0b1596]'>
-                        <a href="#contact">contact</a>
+                    <li className="py-5">
+                        <a className="uppercase text-white hover:underline" href="#contact">contact</a>
                     </li>
                 </ul>
             </div>
         </div>
-    </nav>
+    </div>
   )
 }
 
-export default NavBar
+export default NavBar;
